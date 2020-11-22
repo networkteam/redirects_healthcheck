13bed8b5ed45da09a4aa5a3e5086252f789441a4
Christoph Lehmann <lehmann@networkteam.com>
2020-11-22 22:43:44 +0100

    Cleanup

--- Classes/Service/HealthcheckService.php
+++ Classes/Service/HealthcheckService.php
@@ -18,7 +18,6 @@ use TYPO3\CMS\Core\Site\Entity\Site;
 use TYPO3\CMS\Core\Site\SiteFinder;
 use TYPO3\CMS\Core\Utility\GeneralUtility;
 use TYPO3\CMS\Core\Utility\VersionNumberUtility;
-use TYPO3\CMS\Extbase\Utility\LocalizationUtility;
 use TYPO3\CMS\Frontend\Authentication\FrontendUserAuthentication;
 use TYPO3\CMS\Redirects\Service\RedirectService;
 
